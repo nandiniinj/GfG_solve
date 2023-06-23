e61226056302c6040ddbadd0480907d05d6e292a
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-06-23 10:40:03 +0530

    Updated Solution - GfG to GitHub

--- Basic/Implement Queue using array/implement-queue-using-array.cpp
+++ Basic/Implement Queue using array/implement-queue-using-array.cpp
@@ -79,15 +79,15 @@ void MyQueue :: push(int x)
 //Function to pop an element from queue and return that element.
 int MyQueue :: pop()
 {
-          
-        
         if(front==rear) {
             return -1;
         }
         else {
             int ele= arr[front];
-            
             front++;
             return ele;
+            
+            // return arr[front];
+            // front++;
         }
 }
