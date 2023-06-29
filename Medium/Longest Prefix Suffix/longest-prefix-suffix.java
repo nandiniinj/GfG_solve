0cc1ac94969eb1ebf8f4ba4c7b51bb14c76622d4
Nandini Jain <80736008+nandiniinj@users.noreply.github.com>
2023-06-29 14:42:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int lps(String s) {
+        // code here
+        int len=0;
+        int lps[]=new int [s.length()];
+        int i=1;
+        lps[0]=0;
+        
+        while(i<s.length()){
+            if(s.charAt(i)==s.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return (s.substring(0,len)).length();
+        
+    }
+}
